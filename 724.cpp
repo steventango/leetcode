5956aebd73400dd3a66380b6ca096754915d82ec
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-01-01 15:46:35 -0700

    724 / 1991

---
+++ 724.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int rsum = reduce(nums.begin(), nums.end());
+        int lsum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            rsum -= nums[i];
+            if (lsum == rsum) {
+                return i;
+            }
+            lsum += nums[i];
+        }
+        return -1;
+    }
+};
