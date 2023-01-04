6f50b2dd0a05720d9903dae2c532b1be948572a1
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-01-03 19:36:23 -0700

    21

---
+++ 21.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+   public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr || list2 == nullptr) {
+            return nullptr;
+        }
+        if (list1->val > list2->val) {
+            ListNode* t = list1->next;
+            list1->next = list2;
+            mergeTwoLists(t, list2->next);
+            return list1;
+        } else {
+            ListNode* t = list2->next;
+            list2->next = list1;
+            mergeTwoLists(list1->next, t);
+            return list2;
+        }
+    }
+};
